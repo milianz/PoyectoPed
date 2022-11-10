f705cb2812a97ecf4592ae1108bc69e3c43a69f9
milianz <00053722@uca.edu.sv>
2022-11-10 17:53:21 -0600

    :fire:

--- BlackJack1.cpp
+++ BlackJack1.cpp
@@ -2,19 +2,31 @@
 #include <stdlib.h>
 #include <cstdlib>
 #include <time.h>
-
+#include<windows.h>
+#include <iomanip>
+#include <ctime>
 using namespace std;
+
+//funcion para escoger una palabra de alphabet aleatoreamente
+void NombreCartA(){
+const int array_size=4;
+srand(time(0));
+string alphabet[array_size] ={"Corazones","Diamantes","Treboles","Picas"};
+
+int index= rand()%array_size;
+cout<<" de "<<alphabet[index]<<endl;
+}
 
 //Funcion para generar un numero aleatorio.
 int Random(int min, int max) //rango : [min, max]
 {
-   static bool first = true;
-   if (first) 
-   {  
-      srand( time(NULL) ); 
-      first = false;
-   }
-   return min + rand() % (( max + 1 ) - min);
+static bool first = true;
+    if (first) 
+    {
+        srand( time(NULL) ); 
+        first = false;
+    }
+return min + rand() % (( max + 1 ) - min);
 }
 
 //Funcion para calcular los puntos de la maquina.
@@ -33,7 +45,7 @@ int CartasMaquina(int valor)
             puntosMaquina += 11;
             if(puntosMaquina > 21)
             {
-                puntosMaquina += 1;
+            puntosMaquina += 1;
             }
             carta = 0;
         }
@@ -58,10 +70,10 @@ int CartasMaquina(int valor)
 //Funcion para calcular los puntos del jugador.
 int CartasJugador()
 {
+    int numero;
     int carta = 0, puntosJugador = 0, as = 0;
     
     carta = Random(1, 11);
-    
     //validando el valor de 1.
     if(carta == 1)
     {
@@ -84,6 +96,65 @@ int CartasJugador()
             break;
         }
     }
+    if(carta==2){
+   cout<<"Su numero de carta es 2";
+   NombreCartA();
+   cout<<endl;
+    }
+     if(carta==3){
+        cout<<"Su numero de carta es 3";
+        NombreCartA();
+        cout<<endl;
+        
+    } if(carta==4){
+        cout<<"Su numero de carta es 4";
+        NombreCartA();
+   cout<<endl;
+    }
+     if(carta==5){
+        cout<<"Su numero de carta es 5";
+        NombreCartA();
+   cout<<endl;
+    } 
+    if(carta==6){
+        cout<<"Su numero de carta es 6";
+        NombreCartA();
+   cout<<endl;
+        
+    } 
+    if(carta==7){
+        cout<<"Su numero de carta es 7";
+        NombreCartA();
+   cout<<endl;
+    } 
+    if(carta==8){
+        cout<<"Su numero de carta es 8";
+        NombreCartA();
+   cout<<endl;
+    } 
+    if(carta==9){
+        cout<<"Su numero de carta es 9";
+        NombreCartA();
+   cout<<endl;
+        
+    } 
+    if(carta==10){
+        cout<<"Su numero de carta es 10";
+        NombreCartA();
+   cout<<endl;
+        
+    }
+    if(carta==11){
+        //carta 11 igual a los comodines que hagarra de alphabet 
+        srand(time(NULL));
+        string alphabet = "JQK";
+        char ch = alphabet[rand() % alphabet.size()];
+        cout << "\nObtuvo "<<ch<< endl;
+        NombreCartA();
+        cout<<endl;
+        cout<<"Y el valor de esa carta es 10"<<endl;
+
+    }
     else
     {
         //Si el numero generado es mayor que 10 su valor sera 10.
@@ -92,15 +163,26 @@ int CartasJugador()
             carta = 10;
         }
     }
+    
     puntosJugador += carta;
 
     return puntosJugador;
 }
+//bienvenida Actualizada
+void PantallaDeCarga(){
+    char LCharacter= char(3);
+char LBar[51]={' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+               ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+               ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+               ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',
+               ' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',0};
+int LCounter;
 
-void Bievenida()
-{
-    int continuar = 0;
-    system ("cls");
+Sleep(1000);
+for (LCounter= 0; LCounter<50; LCounter++){
+    LBar[LCounter]=LCharacter;
+    Sleep(100);
+    system("cls");
     cout << "|************************************************************|" << endl;
     cout << "|*********** Bienvenido al Juego Blackjack o 21. ************|" << endl;
     cout << "|                                                            |" << endl;
@@ -115,56 +197,85 @@ void Bievenida()
     cout << "|     cercana a 21.                                          |" << endl;
     cout << "|                                                            |" << endl;
     cout << "|************************************************************|" << endl;
-    cout << "\nDesea continuar? 1||Si 0||No." << endl;
-    cout << "Opcion: ";
-    cin >> continuar;
+    cout << "|"<<setw(40)<<"Cargando Juego..."<<"                    |"<<endl;
+    cout << "|"<<setw(55)<<LBar<<"     |"<<endl;
+    cout << "|************************************************************|";
+}
 
+
+}
+
+void Juego()
+{
+    int continuar = 0;
+    system ("cls");
+    PantallaDeCarga();
+    cout << "\nDesea Jugar? 1||Si 0||No." << endl;
+    cout << "Opcion->";
+    cin >> continuar;
     char opc;
+    int opc2;
     int valor = 0, puntosMaq;
     if(continuar == 1)
     {
-        do
-        {   
-            system("cls");
-            cout << "Desea una carta? s||Si n||No" << endl;
-            cout << "Opcion: ";
-            cin >> opc;
+       cout<<"Desea Jugar contra la maquina o con amigos:\n";
+       cout<<"1- Maquina.\n";
+       cout<<"2- Amigos.\n";
+       cout << "Opcion->";
+       cin >> opc2;
+       if(opc2==1)
+                {
+                    do
+                    {   
+                        system("cls");
+                        cout << "Desea una carta? s||Si n||No" << endl;
+                        cout << "Opcion: ";
+                        cin >> opc;
 
-            if(opc == 's' || opc == 'S')
-            {
-                valor += CartasJugador();
-                cout << "\nPuntos actuales: " << valor << "." << endl; 
+                        if(opc == 's' || opc == 'S')
+                        {
+                            valor += CartasJugador();
+                            cout << "Puntos actuales: " << valor << "." << endl; 
 
-            }
-            if(valor > 21)
-            {
-                cout << "\nDerrota! Excedio 21 puntos." << endl;
-                cout << "\n";
-                exit(1);
-            }
+                        }
+                        if(valor > 21)
+                        {
+                            cout << "\nDerrota! Excedio 21 puntos." << endl;
+                            cout << "\n";
+                            exit(1);
+                        }
 
-            cout << "\n";
-            system ("pause");
+                        cout << "\n";
+                        system ("pause");
 
-        } while (opc == 's' || opc == 'S');
+                    } while (opc == 's' || opc == 'S');
 
-        system("cls");
-        if(valor <= 21)
-        {
-            puntosMaq = CartasMaquina(valor);
-        }
-        if(puntosMaq > valor || puntosMaq == valor)
-        {
-            cout << "\nDerrota! La maquina tiene mayor puntuacion." << endl;
-            cout << "Tu puntuacion: " << valor << " VS " << "Puntuacion de la maquina: " << puntosMaq << endl;
-            cout << "\n";
-        }
+                        system("cls");
+                        if(valor <= 21)
+                        {
+                            puntosMaq = CartasMaquina(valor);
+                        }
+                        if(puntosMaq > valor || puntosMaq == valor)
+                        {
+                            cout << "\nDerrota! La maquina tiene mayor puntuacion." << endl;
+                            cout << "Tu puntuacion: " << valor << " VS " << "Puntuacion de la maquina: " << puntosMaq << endl;
+                            cout << "\n";
+                        }
+                }
+    else if (opc2==2)
+    {
+        cout<<"En proceso.."<<endl;
+    }
+    else{
+        cout<<"esa opcion no existe";
+    }
+    }
+    else{
+        cout<<"no existe";
     }
 }
 
-int main ()
-{
-    Bievenida();
-
-    return 0;
+int main (){
+Juego();
+return 0;
 }
